17d55117fc2856225044ae9df697db11c506d6b8
fzxiehui <xie18960920183@outlook.com>
2025-08-28 22:09:51 +0800

    perf(main): eventlet run project

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from app import create_app
 from app.ws import socketio
-
+import eventlet
+import eventlet.wsgi
 
 if __name__ == "__main__":
 
     app = create_app()
     # app.run()
-    socketio.run(app=app, debug=True)
+    # socketio.run(app=app, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+        use_reloader=False,   # 可选：避免多进程冲突
+    )
