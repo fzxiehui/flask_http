eb6e9967f8c33539c4dc3e344028021b9c7e7323
fzxiehui <xie18960920183@outlook.com>
2025-09-17 13:05:50 +0800

    fix(response): socketio使用jsonify中文件乱码

--- app/utils/response.py
+++ app/utils/response.py
@@ -1,16 +1,29 @@
-from flask import jsonify
+# from flask import jsonify
+# 
+# def success(data=None, msg="ok", code=200):
+#     return jsonify({
+#         "code": code,
+#         "msg": msg,
+#         "data": data
+#     }), code
+# 
+# def error(msg="error", code=400, data=None):
+#     return jsonify({
+#         "code": code,
+#         "msg": msg,
+#         "data": data
+#     }), code
+import json
+from flask import Response
 
 def success(data=None, msg="ok", code=200):
-    return jsonify({
-        "code": code,
-        "msg": msg,
-        "data": data
-    }), code
+    return Response(
+        json.dumps({"code": code, "msg": msg, "data": data}, ensure_ascii=False),
+        mimetype="application/json"
+    ), code
 
 def error(msg="error", code=400, data=None):
-    return jsonify({
-        "code": code,
-        "msg": msg,
-        "data": data
-    }), code
-
+    return Response(
+        json.dumps({"code": code, "msg": msg, "data": data}, ensure_ascii=False),
+        mimetype="application/json"
+    ), code
